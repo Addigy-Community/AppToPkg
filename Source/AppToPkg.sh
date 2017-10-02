cb25f6a02cb3b57628a04199c549a5d28b9be95a
Benjamin Morales <bmorales@gocirrus.com>
2017-10-02 16:26:02 -0500

    umm... not 2015

--- Source/AppToPkg.sh
+++ Source/AppToPkg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2015 Cirrus Partners, LLC
+# Copyright 2017 Cirrus Partners, LLC
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
