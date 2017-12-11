8006495af09767fdad178b0b9d93bfffc74bdc77
Benjamin Morales <benjamin@bwmorales.com>
2017-12-11 05:56:25 -0600

    Major revision; PNG instead of ICNS

--- Source/AppToPkg.sh
+++ Source/AppToPkg.sh
@@ -1,88 +1,82 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Copyright 2017 Cirrus Partners, LLC
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+###############################################################################
+#   Copyright 2017 Benjamin Moralez                                           #
+#                                                                             #
+#   Licensed under the Apache License, Version 2.0 (the "License");           #
+#   you may not use this file except in compliance with the License.          #
+#   You may obtain a copy of the License at                                   #
+#                                                                             #
+#       http://www.apache.org/licenses/LICENSE-2.0                            #
+#                                                                             #
+#   Unless required by applicable law or agreed to in writing, software       #
+#   distributed under the License is distributed on an "AS IS" BASIS,         #
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+#   See the License for the specific language governing permissions and       #
+#   limitations under the License.                                            #
+###############################################################################
 
 function setVariables() {
-    fileName=$(printf "${f##*/}" | sed 's/\.[^.]*$//' )
-#    echo "$fileName"
-    filePath=${f%/*}
-#    echo "$filePath"
-    appVersion=$(/usr/bin/defaults read "${f}/Contents/Info.plist" CFBundleShortVersionString)
-#    echo "$appVersion"
-    shortVersion=$(echo ${appVersion} | cut -d. -f-3)
-#    echo "$shortVersion"
-    pkgName="${fileName}-${shortVersion}.pkg"
-#    echo "$pkgName"
-    workingDir="$HOME/Documents/AppToPKG/${fileName} (${shortVersion})"
-#    echo "$workingDir"
-    appIconName=$(/usr/bin/defaults read "${f}/Contents/Info.plist" CFBundleIconFile | sed -e 's/\.icns$//')
-    appIcon="${f}/Contents/Resources/${appIconName}.icns"
-    addigyInformation="${workingDir}/Addigy Information.txt"
-#    echo "$addigyInformation"
+    FILE_NAME="$(printf "$(basename "${f}")" | sed 's/\.[^.]*$//')"
+    FILE_PATH=${f%/*}
+    APP_VERSION=$(/usr/bin/defaults read "${f}/Contents/Info.plist" CFBundleShortVersionString)
+    SHORT_VERSION=$(echo ${APP_VERSION} | cut -d. -f-3)
+    PKG_NAME="${FILE_NAME}-${SHORT_VERSION}.pkg"
+    WORKING_DIRECTORY="$HOME/Documents/AppToPKG/${FILE_NAME} (${SHORT_VERSION})"
+    ICON_NAME=$(/usr/bin/defaults read "${f}/Contents/Info.plist" CFBundleIconFile | sed -e 's/\.icns$//')
+    APP_ICON="${f}/Contents/Resources/${ICON_NAME}.icns"
+    ADDIGY_INFO="${WORKING_DIRECTORY}/Addigy Information.txt"
 }
 
 function makeContainers() {
-    /bin/mkdir -p "${workingDir}"
-    /usr/bin/touch "${addigyInformation}"
+    /bin/mkdir -p "${WORKING_DIRECTORY}"
+    /usr/bin/touch "${ADDIGY_INFO}"
 }
 
 function printHeader() {
-    printf "Use the following information for your Addigy Custom Software Wizard:\n\n" >> "${addigyInformation}"
-    printf "Software Identifier: '%s'\n\n" "${fileName}" >> "${addigyInformation}"
-    printf "Version: '%s'\n\n" "${shortVersion}" >> "${addigyInformation}"
-    printf "DETAILS:\n" >> "${addigyInformation}"
-    printf "Software Description: <ADD YOUR DESCRIPTION>\n" >> "${addigyInformation}"
+    printf "Use the following information for your Addigy Custom Software Wizard:\n\n" >> "${ADDIGY_INFO}"
+    printf "Software Identifier: '%s'\n\n" "${FILE_NAME}" >> "${ADDIGY_INFO}"
+    printf "Version: '%s'\n\n" "${SHORT_VERSION}" >> "${ADDIGY_INFO}"
+    printf "DETAILS:\n" >> "${ADDIGY_INFO}"
+    printf "Software Description: <ADD YOUR DESCRIPTION>\n" >> "${ADDIGY_INFO}"
 
 }
 
 function gatherICNS() {
-    /bin/cp "${appIcon}" "${workingDir}"
-    printf "Software Icon: " >> "${addigyInformation}"
-    printf "'%s'\n" "${workingDir}/${appIconName}.icns" >> "${addigyInformation}"
-    printf "(If no icon shows, then the app stored its icon in an unconventional manner.)\n\n" \
-    >> "${addigyInformation}"
+    /usr/bin/sips -s format png "${APP_ICON}" --out "${WORKING_DIRECTORY}/${ICON_NAME}.png"
+#    /bin/cp "${APP_ICON}" "${WORKING_DIRECTORY}"
+    printf "Software Icon: " >> "${ADDIGY_INFO}"
+    printf "'%s'\n" "${WORKING_DIRECTORY}/${ICON_NAME}.png" >> "${ADDIGY_INFO}"
 }
 
 function generatePKG() {
-    /usr/bin/productbuild --component "${f}" /Applications "${workingDir}/${pkgName}"
-    printf "UPLOAD FILES: " >> "${addigyInformation}"
-    printf "'%s'\n\n" "${workingDir}/${pkgName}" >> "${addigyInformation}"
+    /usr/bin/productbuild --component "${f}" /Applications "${WORKING_DIRECTORY}/${PKG_NAME}"
+    printf "UPLOAD FILES: " >> "${ADDIGY_INFO}"
+    printf "'%s'\n\n" "${WORKING_DIRECTORY}/${PKG_NAME}" >> "${ADDIGY_INFO}"
 }
 
 function generateInstallScript() {
-    printf "INSTALLATION:\n" >> "${addigyInformation}"
+    printf "INSTALLATION:\n" >> "${ADDIGY_INFO}"
     printf '/usr/sbin/installer -pkg "/Library/Addigy/ansible/packages/%s (%s)/%s" -target /\n\n' \
-    "${fileName}" "${shortVersion}" "${pkgName}" >> "${addigyInformation}"
+    "${FILE_NAME}" "${SHORT_VERSION}" "${PKG_NAME}" >> "${ADDIGY_INFO}"
 }
 
 function generateConditionalScript() {
-    printf "CONDITIONS:\n" >> "${addigyInformation}"
-    printf "Install on Success: True (on)\n" >> "${addigyInformation}"
-    printf "Check for Application Version:\n" >> "${addigyInformation}"
-    printf "\tSet: Success\n" >> "${addigyInformation}"
+    printf "CONDITIONS:\n" >> "${ADDIGY_INFO}"
+    printf "Install on Success: True (on)\n" >> "${ADDIGY_INFO}"
+    printf "Check for Application Version:\n" >> "${ADDIGY_INFO}"
+    printf "\tSet: Success\n" >> "${ADDIGY_INFO}"
     printf "\tif the following installed application: /Applications/%s.app\n" \
-    "${fileName}" >> "${addigyInformation}"
-    printf "\thas version older than\n" >> "${addigyInformation}"
-    printf "\tVersion: '%s'\n\n" "$appVersion" >> "${addigyInformation}"
+    "${FILE_NAME}" >> "${ADDIGY_INFO}"
+    printf "\thas version older than\n" >> "${ADDIGY_INFO}"
+    printf "\tVersion: '%s'\n\n" "$APP_VERSION" >> "${ADDIGY_INFO}"
 }
 
 function generateUninstallScript() {
-    printf "REMOVE SCRIPT:\n" >> "${addigyInformation}"
-    printf '/bin/rm -Rf "/Applications/%s.app"\n' "${fileName}" >> "${addigyInformation}"
+    printf "REMOVE SCRIPT:\n" >> "${ADDIGY_INFO}"
+    printf '/bin/rm -Rf "/Applications/%s.app"\n' "${FILE_NAME}" >> "${ADDIGY_INFO}"
     printf '/bin/rm -Rf "/Library/Addigy/ansible/packages/%s (%s)"' \
-    "${fileName}" "${shortVersion}" >> "${addigyInformation}"
+    "${FILE_NAME}" "${SHORT_VERSION}" >> "${ADDIGY_INFO}"
 
 }
 
